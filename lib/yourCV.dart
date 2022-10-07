85f4f54980758a8687a7fc49a9eb9fc26718c7fb
Oloow22 <okangawilliam2000@gmail.com>
2022-10-07 19:44:02 +0300

    Add

--- lib/yourCV.dart
+++ lib/yourCV.dart
@@ -13,75 +13,77 @@ class _YourCvState extends State<YourCv> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purple.shade50,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(hintText: 'Phone Number'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(hintText: 'Phone Number'),
+                ),
+            ),
+      
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(hintText: 'Residence'),
+                   ),
               ),
-          ),
-
+             SizedBox( height: 20,),
+             Text(
+              'Academic Backgroung',
+              style:  GoogleFonts.acme(
+                        color: Colors.purple.shade800,
+                        fontSize: 20,
+                        fontStyle: FontStyle.normal
+                      ),
+            ),
+            
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(hintText: 'Residence'),
-                 ),
+                minLines: 5,
+                maxLines: 10,
+                decoration: InputDecoration(
+                border: OutlineInputBorder()
+              )),
+            ), 
+      
+          Text(
+              'Work experience',
+              style:  GoogleFonts.acme(
+                        color: Colors.purple.shade800,
+                        fontSize: 20,
+                        fontStyle: FontStyle.normal
+                      ),
             ),
-           SizedBox( height: 20,),
-           Text(
-            'Academic Backgroung',
-            style:  GoogleFonts.acme(
-                      color: Colors.purple.shade800,
-                      fontSize: 20,
-                      fontStyle: FontStyle.normal
-                    ),
-          ),
-          
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              minLines: 5,
-              maxLines: 10,
-              decoration: InputDecoration(
-              border: OutlineInputBorder()
-            )),
-          ), 
-
-        Text(
-            'Work experience',
-            style:  GoogleFonts.acme(
-                      color: Colors.purple.shade800,
-                      fontSize: 20,
-                      fontStyle: FontStyle.normal
-                    ),
-          ),
-          SizedBox(  height: 20, ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              minLines: 5,
-              maxLines: 10,
-              decoration: InputDecoration(
-              border: OutlineInputBorder()
-            )),
-          ), 
-
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.indigo.shade300),
-          onPressed: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => Success()));
-        }, child: Text(
-          style: GoogleFonts.acme(
-                      color: Colors.grey.shade50,
-                      fontSize: 20,
-                      fontStyle: FontStyle.normal
-                    ),
-          'Submit my application'))
-
-        ],
+            SizedBox(  height: 20, ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                minLines: 5,
+                maxLines: 10,
+                decoration: InputDecoration(
+                border: OutlineInputBorder()
+              )),
+            ), 
+      
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.indigo.shade300),
+            onPressed: (){
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => Success()));
+          }, child: Text(
+            style: GoogleFonts.acme(
+                        color: Colors.grey.shade50,
+                        fontSize: 20,
+                        fontStyle: FontStyle.normal
+                      ),
+            'Submit my application'))
+      
+          ],
+        ),
       ),
       appBar: AppBar(
         backgroundColor: Colors.purple.shade800,
